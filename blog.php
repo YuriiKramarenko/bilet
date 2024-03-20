964bcbc771191836ad27e2752e9b7c0268cb007b
YuriiKramarenko <kramarenkoyurii2137@gmail.com>
2024-03-20 13:27:15 +0100

    koniec zajęć 20.03.24

--- blog.php
+++ blog.php
@@ -5,7 +5,15 @@ $password = "";
 $dbname = "bilet_1k";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-  die("Połączenie nieudane: ". $conn->connect_error);
+  die ("Połączenie nieudane: " . $conn->connect_error);
 }
+$sql = "SELECT * FROM PRZEPISY";
+$result = $conn->query($sql);
+echo "<ol>";
+while ($row = $result->fetch_assoc()) {
 
+  echo "<li>" . $row["tytuł"] . "</li>";
+}
+echo "</ol>";
+$conn->close();
 ?>
