44314ad75adaafab3b6157fb7f011b418e50e664
YuriiKramarenko <kramarenkoyurii2137@gmail,com>
2024-04-10 00:28:34 +0200

    dodanie loginu

--- blog.php
+++ blog.php
@@ -2,18 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bilet_1k";
+$dbname = "bilet";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die ("Połączenie nieudane: " . $conn->connect_error);
 }
-$sql = "SELECT * FROM PRZEPISY";
-$result = $conn->query($sql);
-echo "<ol>";
-while ($row = $result->fetch_assoc()) {
-
-  echo "<li>" . $row["tytuł"] . "</li>";
-}
-echo "</ol>";
 $conn->close();
 ?>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.1.css"> 
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+    <body>
+        <div class="wrapper">
+<from action="">
+            <h1>login</h1>
+        <div class="input-box">
+        <input type="text" placeholder="Username"
+        required>
+        <i class='bx bxs-user'></i>
+    </div>
+        <div class="input-box">
+        <input type="password" 
+        placeholder="Password"required>
+        <i class='bx bxs-lock-alt'></i>
+    </div>
+
+    <div class="remember-forgot">
+        <label><imput type="checbox"> Remember me
+        </label>
+        <a href="#">Forgot password?<a>
+    </div>
+
+    <button type="sumbit" class="btn">login</button>
+
+    <div class="register-list">
+        <p>don't have an account? <a href="#">Register</a></p>
+    </div>
+</from>
+    </div>
+         
+    </body>
+</html>
