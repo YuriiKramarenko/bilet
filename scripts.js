e68dc20660f8029fbbab3357929b4b0ee7338c74
KramarenkoYurii <kramarenkoyurii2137@gmail.com>
2024-05-22 11:08:19 +0200

    dodanie

--- scripts.js
+++ scripts.js
@@ -1,30 +1,49 @@
-let isDown = false;
-let startX;
-let scrollLeft;
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu-button');
+    const menu = document.getElementById('side-menu');
 
-const container = document.querySelector('.container');
+    // Ustawienie pozycji menu (przykład: top: 100px, left: 50px)
+    menu.style.top = '100px';
+    menu.style.left = '50px';
 
-container.addEventListener('mousedown', (e) => {
-    isDown = true;
-    container.style.cursor = 'grabbing';
-    startX = e.pageX - container.offsetLeft;
-    scrollLeft = container.scrollLeft;
-});
+    menuButton.addEventListener('click', () => {
+        menu.classList.toggle('menu-open');
+        console.log('Menu button clicked');
+    });
 
-container.addEventListener('mouseleave', () => {
-    isDown = false;
-    container.style.cursor = 'grab';
-});
+    // Kod obsługi przeciągania kontenera z obrazami
+    let isDown = false;
+    let startX;
+    let scrollLeft;
 
-container.addEventListener('mouseup', () => {
-    isDown = false;
-    container.style.cursor = 'grab';
-});
+    const container = document.querySelector('.container');
 
-container.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - container.offsetLeft;
-    const walk = (x - startX) * 2;
-    container.scrollLeft = scrollLeft - walk;
+    container.addEventListener('mousedown', (e) => {
+        isDown = true;
+        container.style.cursor = 'grabbing';
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+        console.log('Mouse down');
+    });
+
+    container.addEventListener('mouseleave', () => {
+        isDown = false;
+        container.style.cursor = 'grab';
+        console.log('Mouse leave');
+    });
+
+    container.addEventListener('mouseup', () => {
+        isDown = false;
+        container.style.cursor = 'grab';
+        console.log('Mouse up');
+    });
+
+    container.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - container.offsetLeft;
+        const walk = (x - startX) * 2;
+        container.scrollLeft = scrollLeft - walk;
+        console.log('Mouse move');
+    });
 });
--- strona.php
+++ strona.php
@@ -1,11 +1,9 @@
 <?php
-
 session_start();
 
-if (!isset($_SESSION['zalogowany']))
-{
-  header('Location: index.php');
-  exit();
+if (!isset($_SESSION['zalogowany'])) {
+    header('Location: index.php');
+    exit();
 }
 ?>
 
@@ -15,72 +13,67 @@ if (!isset($_SESSION['zalogowany']))
     <title>Document</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="menu-bar">
-    <a id="menu">MENU</a>
-    <a id="bell">&</a>
-    <a id="nearby">W POBLIŻU</a>
-  </div>
-<div class="login-container">
-  <div>
-    <?php echo '[<a href="logout.php">Wyloguj się</a>]'; ?>
-  </div>
-  <div class="profile">
-    <div>
-    <img src="img/logotip.jpg" alt="logotip" id="logotip">
-    </div>
-    <div>
-    <samp id="kolo"><img src="img/avatar.jpg" alt="Avatar" class="avatar"></samp>
-    </div>
-    <div class="info">
-  </br>
-      <?php
-      
-      echo $_SESSION['user'];
-      echo "</br>".$_SESSION['code'];
+    <div class="menu-bar">
+        <!-- Panel Menu -->
+        <div id="side-menu" class="menu">
+            <h2>Menu</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </div>
 
-      ?>
+        <!-- Main Content -->
+        <button id="menu-button">Menu</button>
+        <a id="bell" href="pruby.html">&</a>
+        <a id="nearby" href="#">W POBLIŻU</a>
     </div>
-    <div>
-    <img src="img/qrcode.png" alt="QR code" class="qrcode">
+
+    <div class="login-container">
+        <div>
+            <?php echo '[<a href="logout.php">Wyloguj się</a>]'; ?>
+        </div>
+        <div class="profile">
+            <img src="img/logotip.jpg" alt="logotip" id="logotip">
+            <samp id="kolo"><img src="img/avatar.jpg" alt="Avatar" class="avatar"></samp>
+            <div class="info">
+                <?php
+                echo $_SESSION['user'];
+                echo "<br>" . $_SESSION['code'];
+                ?>
+            </div>
+            <img src="img/qrcode.png" alt="QR code" class="qrcode">
+        </div>
+        <div class="container">
+            <div class="image"><img src="img/luffy.jpg" alt="Obrazek 1"></div>
+            <div class="image"><img src="img/zoro.jpg" alt="Obrazek 2"></div>
+            <div class="image"><img src="img/brook.jpg" alt="Obrazek 3"></div>
+            <div class="image"><img src="img/franky.jpg" alt="Obrazek 4"></div>
+        </div>
+        <div class="tickets">
+            <p>POSIADANE BILETY</p>
+        </div>
+        <div id="pay">
+            <button id="kup">KUP BILET</button>
+        </div>
+        <div>
+            <p>POSIADANE PAKIETY</p>
+        </div>
+        <div id="pay">
+            <button id="dodaj">DODAJ PAKIET</button>
+        </div>
+        <div class="pakiet">
+            <p>2023/2024 Roczny pakiet<br> bezpłatnych przejazdów</p>
+            <p>ważny od 2023-10-05</p>
+            <p>ważny do 2024-10-10</p>
+        </div>
     </div>
-  </div>
-  <div class="container">
-    <div class="image"><img src="img/luffy.jpg" alt="Obrazek 1"></div>
-    <div class="image"><img src="img/zoro.jpg" alt="Obrazek 2"></div>
-    <div class="image"><img src="img/brook.jpg" alt="Obrazek 3"></div>
-    <div class="image"><img src="img/franky.jpg" alt="Obrazek 4"></div>
-  </div>
-  <script src="scripts.js"></script>
-  <div class="tickets">
-    <p class="tickets text">POSIADANE BILETY</p>
-  </div>
-  <div id="pay">
-    <button id="kup">
-      <a href="kupbilet.php">KUP BILET</a>
-    </button>
-  </div>
-  <div>
-    <p class="tickets text">POSIADANE PAKIETY</p>
-</div>
-<div id="pay">
-  <button id="dodaj">
-    <p>DODAJ PAKIET</p>
-    </button>
-  </div>
-  <div class="pakiet">
-    <p>2023/2024 Roczny pakiet bezpłatnych przejazdów</p>
-    <p>ważny od 2023-10-05</p>
-    <p>ważny do 2024-10-10</p>
-  </div>
-</div>
-<div class="footer">
-    <a id="q">1</a>
-    <a id="w">2</a>
-    <a id="e">3</a>
-    <a id="r">4</a>
-  </div>
+
+    <script src="scripts.js"></script>
 </body>
 </html>
